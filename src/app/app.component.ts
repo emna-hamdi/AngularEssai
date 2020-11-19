606c81a4c8b66578a0f6790df24e2b513dd3a569
emna-hamdi <emna.hamdi.adressemail@gmail.com>
2020-11-19 22:38:33 +0100

    nouveau projet

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'applicationAngular';
+  bgColor="yellow";
+  show:Boolean =false;
+  changeStatus(){
+    this.show = !this.show;
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,30 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ChildAComponent } from './child-a/child-a.component';
+import { ChildBComponent } from './child-b/child-b.component';
+
+import { ReactiveFormsModule } from '@angular/forms';
+import { RegistrationComponent } from './registration/registration.component';
+import { AuthentificationComponent } from './authentification/authentification.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+// import {bootstrap} from "./bootstrap";
+// import { FontAwesomeModule } from '@fortawesome/angular-fontawesome'
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ChildAComponent,
+    ChildBComponent,
+    RegistrationComponent,
+    AuthentificationComponent,
+
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
